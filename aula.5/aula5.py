2a4291f799ee4e108f4b26453bc0af85a311a1f2
brunamss <brunamss54@gmail.com>
2025-10-28 21:32:55 -0300

    Add files via upload

---
+++ aula.5/aula5.py
@@ -0,0 +1,28 @@
+# 9 Crie um sistema de cadastro com as estruturas 
+# que vc já conhece(Use apenas input, print e variavel)
+
+
+# nome, email, idade, cidade, gradução, estado civil
+
+
+#cadastro pessoal
+
+nome = input ('Digite o Nome: ')
+
+email = input ('Digite seu Email: ')
+
+idade = input ('Digite sua Idade:')
+
+cidade = input ('Digite sua Cidade: ')
+
+graduação = input ('Digite sua Graduação:')
+
+estado_civil = input ('Digite seu Estado civil: ')
+
+
+print (nome)
+print (email)
+print (idade)
+print (cidade)
+print (graduação)
+print (estado_civil)
