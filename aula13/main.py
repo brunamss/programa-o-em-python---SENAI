5202adfb083f95a8eb5a5a6d42153dfb84afb245
brunamss <brunamss54@gmail.com>
2025-11-12 21:40:12 -0300

    Add files via upload

---
+++ aula13/main.py
@@ -0,0 +1,15 @@
+import blocosDeCódigo  as bc
+
+print(bc.aleatorio())
+
+print(bc.aleatorio2())
+
+print(bc.aleatorio3())
+
+print(bc.contagem())
+
+bc.numero()
+
+bc.multiplicação()
+
+bc.contagem1()
---
+++ aula13/modulo.py
@@ -0,0 +1,52 @@
+# main.py
+
+# import meu_modulo
+
+
+
+# def estatistica():
+#     lista = [1,2,3,5,6,10,10]
+
+
+#     moda =  meu_modulo.moda(lista)
+#     print('moda:', moda)
+
+
+
+#     des =  meu_modulo.desvio(lista)
+#     print('Desvio:', des)
+
+
+
+#     media =  meu_modulo.media(lista)
+#     print('media:', media)
+
+
+
+#     maior, menor =  meu_modulo.menor_maior(lista)
+#     print('maior:', maior,'menor:',menor )
+
+
+# estatistica()    
+
+
+# # meu_modulo.py
+
+# import statistics
+
+
+
+# def moda(lista):
+#     return statistics.mode(lista)
+
+
+# def media(lista):
+#     return statistics.mean(lista)
+    
+# def desvio(lista):
+#     return statistics.stdev(lista)
+
+
+# def menor_maior(lista):
+#     return max(lista),min(lista)
+
