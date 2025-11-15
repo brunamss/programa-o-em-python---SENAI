717cbc70aac3bbec86ad3962c651df3130850741
brunamss <brunamss54@gmail.com>
2025-11-14 21:28:14 -0300

    Add files via upload

---
+++ aula17/test_minhas_funções.py
@@ -0,0 +1,6 @@
+import minhas__funções
+
+def test_somar():
+       assert minhas__funções.somar(2, 3) == 5
+       assert minhas__funções.somar(5, 7) == 12
+       assert minhas__funções.somar(10, -3) == 7
