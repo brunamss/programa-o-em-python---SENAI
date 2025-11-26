69703f4710a5d2110ac4827ac1f5eb3d46196a0a
MARI <brunamss54@gmail.com>
2025-11-25 21:49:44 -0300

    Add files via upload

---
+++ bancodeDados/bancoDados.py
@@ -0,0 +1,21 @@
+# import sqlite3
+
+# #CRUD
+# # create - read - update - delete 
+
+# con = sqlite3.connect('banco.db')
+# c = con.cursor()
+
+# c. execute(''' CREATE TABLE IF NOT EXISTS tabela(
+           
+#            nome TEXT,
+#            idade INTEGER
+
+
+
+# )''')
+
+# c.execute('INSERT INTO tabela VALUES(?,?)', ('julia', 40))
+# con.commit()
+
+# con.close()
